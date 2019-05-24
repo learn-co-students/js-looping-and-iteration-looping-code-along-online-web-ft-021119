05830cc34b6cbea4f1be91595f369e84f2a815bf
joanrig <github email address>
2019-05-24 13:02:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+
+function writeCards(people, occasion) {
+  let cards = []
+  for (let i = 0; i < people.length; i++) {
+    cards.push( `Thank you, ${people[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return cards
+}
+
+
+
+function countdown(start) {
+  for (let i =start; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
+ 

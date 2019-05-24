95eb5bb2c18abfc7eba742788046d24b70f4631a
bbennett7 <bryn.bennett@gmail.com>
2019-05-24 09:21:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let thankYous = []
+  for (let i = 0; i < names.length; i++) {
+    thankYous[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+  return thankYous;
+}
+
+function countdown(startNumber) {
+  while (startNumber >= 0) {
+    console.log(startNumber--)
+  }
+}

c221f52e97659a6c532e710d0047df1e2b6d3e91
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-24 12:45:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+const cardArray = [];
+ 
+function writeCards(names, event) {
+    
+  for (let i = 0; i < names.length; i++) {
+    cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+ return names;
+}
+ 
+writeCards(names, event);
